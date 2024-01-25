d4318b1d1cc25cb9355fa225dfc8ec7c9269dad3
Julia-Zykova <juli.zikova@yandex.ru>
2024-01-25 15:30:49 +0300

    Modify Ajax (remove extra-posts), separate ajax in js file

--- models_app/models/comment/models.py
+++ models_app/models/comment/models.py
@@ -8,13 +8,13 @@ class Comment(BaseModel):
 	user = models.ForeignKey('CustomUser', on_delete=models.CASCADE, verbose_name='Автор', related_name = 'comments')
 	text = models.CharField(max_length=200)
 	comment = models.ForeignKey ('Comment',on_delete=models.CASCADE,
-		verbose_name='Ответ', related_name = 'replies', blank = True, null=True)
+		verbose_name='Ответ', related_name = 'comments', blank = True, null=True)
 	
 
 	class Meta:
 		verbose_name = 'Комментарий'
 		verbose_name_plural = 'Комментарии'
-		ordering = ['user', 'photo']
+		ordering = ["created_at"]
 
 	def __str__(self):
 		return self.text
