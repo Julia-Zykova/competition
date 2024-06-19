3a2e6a8fd65ca8945edde8d792b9c47e0567da91
Yulia Zikova <juli.zikova@yandex.ru>
2024-06-19 11:53:07 +0300

    Added sticky footer, some admin settings,del permission_classes in view,

--- models_app/admin.py
+++ models_app/admin.py
@@ -8,6 +8,8 @@ from django.utils.safestring import mark_safe
 from .models import *
 from django import forms
 
+from drf.tasks import reject_photo
+
 
 @admin.register(CustomUser)
 class UserAdmin(admin.ModelAdmin):
@@ -15,16 +17,19 @@ class UserAdmin(admin.ModelAdmin):
     fields = ['email', 'last_name', 'first_name', 'user_photo', 'id']
     readonly_fields = ['email', 'last_name', 'first_name', 'user_photo', 'id', 'personal_photo']
 
-    def personal_photo(self, obj):
+    @staticmethod
+    def personal_photo(obj):
         return mark_safe(f'<img src="{obj.user_photo.url}">')
 
 
 class VoiceInline(admin.TabularInline):
     model = Voice
+    extra = 0
 
 
 class CommentInline(admin.TabularInline):
     model = Comment
+    extra = 0
 
 
 class PhotoAdminForm(forms.ModelForm):
@@ -33,14 +38,25 @@ class PhotoAdminForm(forms.ModelForm):
         fields = "__all__"
 
     def clean_state(self):
-        if self.cleaned_data['state'] == 'in_moderation':
+        if self.initial['state'] in ['approved', 'on_delete'] and self.cleaned_data['state'] == 'in_moderation':
             raise forms.ValidationError('Вы не можете направить фото на повторную модерацию')
         if self.initial['state'] == 'approved' and self.cleaned_data['state'] == 'rejected':
             raise forms.ValidationError('Вы не можете отклонить одобренное фото')
         elif self.initial['state'] == 'rejected' and self.cleaned_data['state'] == 'approved':
             raise forms.ValidationError('Вы не можете одобрить отклоненное фото')
-        elif self.initial['state'] == 'on_delete' and self.cleaned_data['state'] in ['in_moderation', 'approved', 'rejected']:
+        elif self.initial['state'] == 'on_delete' and self.cleaned_data['state'] in ['in_moderation', 'approved',
+                                                                                     'rejected']:
             raise forms.ValidationError('Вы не можете сменить статус этого фото')
+        elif self.initial['state'] == 'in_moderation' and self.cleaned_data['state'] == 'rejected':
+            import environ
+            env = environ.Env()
+
+            result = reject_photo.apply_async(
+                args=[self.instance.id],
+                countdown=int(env('TIME_BEFORE_REJECT'))
+            )
+            return self.initial['state']
+
         return self.cleaned_data["state"]
 
 
@@ -48,10 +64,15 @@ class PhotoAdminForm(forms.ModelForm):
 class PhotoAdmin(admin.ModelAdmin):
     form = PhotoAdminForm
     list_display = ('title', 'author', 'pub_date', 'id', 'state')
+    list_display_links = ('title', 'id')
+    list_editable = ('state',)
     fields = ['state', 'author', 'title', 'pub_date', 'id', 'post_photo', 'image']
     readonly_fields = ['author', 'title', 'pub_date', 'id', 'post_photo', 'image']
     search_fields = ['author', 'title']
     list_filter = ['pub_date', 'state', 'title']
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
     def save_model(self, request, obj, form, change):
         if 'state' in form.changed_data:
@@ -61,15 +82,14 @@ class PhotoAdmin(admin.ModelAdmin):
             if obj.state == "approved":
                 message = f'Ваше фото "{obj.title}" было одобрено'
                 obj.pub_date = datetime.datetime.now()
-            elif obj.state == "rejected":
-                message = f'Ваше фото "{obj.title}" было отклонено'
-            async_to_sync(channel_layer.group_send)(
-                'user_' + str(author.id),
-                {
-                    'type': 'user.message',
-                    'message': message
-                }
-            )
+                async_to_sync(channel_layer.group_send)(
+                    'user_' + str(author.id),
+                    {
+                        'type': 'user.message',
+                        'message': message
+                    }
+                )
+
         super().save_model(request, obj, form, change)
 
     def post_photo(self, obj):
@@ -94,8 +114,18 @@ class PhotoAdmin(admin.ModelAdmin):
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('user', 'text', 'id')
+    fields = ['user', 'text', 'photo', 'comments', 'id']
+    readonly_fields = ['user', 'text', 'photo', 'comments', 'id']
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 @admin.register(Voice)
 class VoiceAdmin(admin.ModelAdmin):
     list_display = ('user', 'photo', 'id')
+    fields = ['user', 'photo', 'id']
+    readonly_fields = ['user', 'photo', 'id']
+
+    def has_delete_permission(self, request, obj=None):
+        return False
--- photo_app/views/photo/views.py
+++ photo_app/views/photo/views.py
@@ -22,8 +22,6 @@ from models_app.models.photo.models import Photo
 class ListPhotoView(View):
     template_name = 'photo_app/list_of_photos.html'
 
-    #permission_classes = (IsAuthenticatedOrReadOnly)
-
     def get(self, request):
         outcome = ServiceOutcome(
             ListOfPhotoService, request.GET.dict() | {
@@ -46,7 +44,6 @@ class ListPhotoView(View):
 
 
 class DetailPhotoView(View):
-    #permission_classes = (IsAuthenticatedOrReadOnly) 
 
     def get(self, request, **kwargs):
         outcome = ServiceOutcome(
@@ -70,26 +67,27 @@ class DetailPhotoView(View):
 class UploadPhotoView(View):
     template_name = 'photo_app/upload_photos.html'
 
-    #permission_classes = (IsAuthenticated)
-
     def get(self, request, *args, **kwargs):
         get_channel_layer()
         return render(request, self.template_name, context={"form": UploadPhotoForm()})
 
     def post(self, request):
-        outcome = ServiceOutcome(
-            UploadPhotoService, request.POST.dict() | {
-                'author': request.user if self.request.user.is_authenticated else None
-            }, request.FILES.dict()
-        )
+        form = UploadPhotoForm(request.POST)
+        if form.is_valid():
+            outcome = ServiceOutcome(
+                UploadPhotoService, request.POST.dict() | {
+                    'author': request.user if self.request.user.is_authenticated else None
+                }, request.FILES.dict()
+            )
 
-        return redirect('photo_app:detail', photo=outcome.result.id)
+            return redirect('photo_app:detail', photo=outcome.result.id)
+        else:
+
+            return render(request, self.template_name, context={"form": form})
 
 
 class EditPhotoView(View):
     template_name = 'photo_app/edit_photo.html'
-
-    #permission_classes = (IsAuthenticated)
 
     def get(self, request, *args, **kwargs):
         get_channel_layer()
@@ -104,7 +102,6 @@ class EditPhotoView(View):
 
 
 class DeletePhotoView(View):
-    #permission_classes = (IsAuthenticated)
 
     def post(self, request, **kwargs):
         outcome = ServiceOutcome(
@@ -113,7 +110,7 @@ class DeletePhotoView(View):
 
 
 class RestorePhotoView(View):
-    #permission_classes = (IsAuthenticated)
+
     def post(self, request, **kwargs):
         outcome = ServiceOutcome(
             RestorePhotoService, request.POST.dict() | {'photo': self.kwargs['photo']})
