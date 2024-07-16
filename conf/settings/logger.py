0eb132b57a0b1e5a2d7d9bb2ac02c7595f58aa71
Yulia Zikova <juli.zikova@yandex.ru>
2024-07-16 18:57:23 +0300

    Added loggers settings

--- conf/settings/logger.py
+++ conf/settings/logger.py
@@ -1,38 +1,62 @@
 import logging.config
 import environ
 
+from conf.settings.django import BASE_DIR
+
 env = environ.Env()
 
-logging.basicConfig(
-    filename=env("LOG_FILE_NAME"),
-    level=getattr(logging, env("LOG_LEVEL")),
-    format=env("LOG_FORMAT"),
-    datefmt=env("LOG_DATETIME_FORMAT"),
-)
 
-# LOGGING_CONFIG = None
-#
-# LOGGING = {
-#     "version": 1,
-#     "disable_existing_loggers": False,
-#     "handlers": {
-#         "file": {
-#             "level": env("LOG_LEVEL"),
-#             "class": "logging.FileHandler",
-#             "filename": env("LOG_FILE_NAME"),
-#         },
-#     },
-#     "root": {
-#         "handlers": ["console"],
-#         "level": env("LOG_LEVEL_ROOT"),
-#     },
-#     "loggers": {
-#         "django": {
-#             "handlers": ["file"],
-#             "level": env("LOG_LEVEL"),
-#             "propagate": True,
-#         },
-#     },
-# }
-#
-# logging.config.dictConfig(LOGGING)
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} - {asctime} - {module} - {funcName}: {message}",
+            "datefmt": env("LOG_DATETIME_FORMAT"),
+            "style": "{",
+        },
+        "simple": {
+            "format": "{levelname} {message}",
+            "datefmt": env("LOG_DATETIME_FORMAT"),
+            "style": "{",
+        },
+    },
+    "filters": {
+        "require_debug_true": {
+            "()": "django.utils.log.RequireDebugTrue",
+        },
+    },
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "filters": ["require_debug_true"],
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
+        },
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": BASE_DIR + env("LOG_FILE_NAME"),
+            "formatter": "verbose",
+        },
+    },
+    "root": {
+            "handlers": ["console"],
+            "level": getattr(logging, env("LOG_LEVEL_ROOT")),
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "propagate": True,
+        },
+        "django.request": {
+            "handlers": ["console", "file"],
+            "level": getattr(logging, env("LOG_LEVEL_REQUEST")),
+            "propagate": False,
+        },
+        "myproject.custom": {
+            "handlers": ["console", "file"],
+            "level": getattr(logging, env("LOG_LEVEL_CUSTOM")),
+        },
+    },
+}
