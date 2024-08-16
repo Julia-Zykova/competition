863f90f433d4ddd408252d41665f91c7610c4a22
Yulia Zikova <juli.zikova@yandex.ru>
2024-08-16 10:09:32 +0300

    Moved task for inactive users to conf/celery

--- conf/celery.py
+++ conf/celery.py
@@ -2,18 +2,29 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
 app = Celery('conf')
 app.config_from_object('django.conf:settings')
 # Load task modules from all registered Django app configs. 
+
 app.autodiscover_tasks()
 
+
+@app.task(task_reject_on_worker_lost=True, name='delete_inactive_users')
+def delete_inactive_users():
+    from models_app.models import CustomUser
+
+    users = CustomUser.objects.prefetch_related("voices", "comments", "photos").filter(is_active=False)
+
+    for user in users:
+        if user.voices.count() == 0 and user.comments.count() == 0 and user.photos.count() == 0:
+            user.delete()
+
+
 app.conf.beat_schedule = {
-    # Executes every Monday morning at 7:30 a.m.
-    'add-every-day': {
-        'task': 'tasks.delete_inactive_users',
-        'schedule': crontab(hour=13, minute=50),
+    'delete_inactive_users': {
+        'task': 'delete_inactive_users',
+        'schedule': crontab(hour=9, minute=57),
     },
 }
 app.conf.timezone = 'Europe/Moscow'
